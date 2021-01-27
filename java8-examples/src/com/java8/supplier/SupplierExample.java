4940c19bf9bf620c26e289a88d27617263506f25
Prasad <bprasad2472@gmail.com>
2021-01-27 12:39:49 +0530

    Supplier Example

--- java8-examples/src/com/java8/supplier/SupplierExample.java
+++ java8-examples/src/com/java8/supplier/SupplierExample.java
@@ -1,5 +1,8 @@
 package com.java8.supplier;
 
+import java.time.LocalDate;
+import java.util.function.Supplier;
+
 public class SupplierExample {
 
 	public static void main(String[] args) {
@@ -9,6 +12,16 @@ public class SupplierExample {
 		 * T - the type of results supplied by
 		 * this supplier
 		 */
+		//Supplier<ArrayList<Integer>> supplier = ArrayList::new;
+		//Supplier<List<Employee>> empSupplier = ArrayList<Employee>::new;
+		
+		Supplier<Double> randomNumber = Math::random;
+		System.out.println("randomNumber:"+randomNumber.get());
+		
+		Supplier<LocalDate> local = LocalDate::now;
+		local = () -> LocalDate.now();
+		
+		System.out.println("LocalDateTime:"+local.get());
 	}
 
 }
