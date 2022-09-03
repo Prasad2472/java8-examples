aefb712eb2684790330c783bfd01347ab405a766
Prasad2472 <34393472+Prasad2472@users.noreply.github.com>
2022-09-03 14:02:59 +0530

    Update LinearSearch.java

--- java8-examples/src/com/core/interview/programs/LinearSearch.java
+++ java8-examples/src/com/core/interview/programs/LinearSearch.java
@@ -1,7 +1,10 @@
 package com.core.test;
 
 public class LinearSearch {
-
+/**
+Linear Search is defined as a sequential search algorithm that starts at one end and goes through each element of a list until the desired element is found, 
+otherwise the search continues till the end of the data set
+**/
 	public static void main(String[] args) {
 		int[] arr = { 5, 40, 6, 3, 84, 25, 44 };
 		System.out.println(findElement(arr, 155));
